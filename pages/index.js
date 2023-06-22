bc91bf67a809ace67229329ef52a026031a5c033
Saksham Mishra <sakmis555@gmail.com>
2023-06-22 20:53:05 +0530

    added: UI of chat page

--- pages/index.js
+++ pages/index.js
@@ -18,16 +18,10 @@ export default function Home() {
           {user && <Link href="/api/auth/logout">Logout</Link>}
           {!user && (
             <>
-              <Link
-                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-                href="/api/auth/login"
-              >
+              <Link className="btn" href="/api/auth/login">
                 Login
               </Link>
-              <Link
-                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-                href="/api/auth/signup"
-              >
+              <Link className="btn" href="/api/auth/signup">
                 Signup
               </Link>
             </>

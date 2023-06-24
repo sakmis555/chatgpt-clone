5ec316a527870d36947dc2c7bdd48b21ec5f2aa6
Saksham Mishra <sakmis555@gmail.com>
2023-06-24 14:36:08 +0530

    feat: display message component in chat UI

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,13 +1,28 @@
 import { ChatSidebar } from "components/ChatSidebar";
+import { Message } from "components/Message";
 import Head from "next/head";
 import { streamReader } from "openai-edge-stream";
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function ChatPage() {
   const [messageText, setMessageText] = useState("");
   const [incomingMessage, setIncomingMessage] = useState("");
+  const [newChatMessages, setNewChatMessages] = useState([]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setNewChatMessages((prev) => {
+      const newChatMessages = [
+        ...prev,
+        {
+          _id: uuid(),
+          role: "user",
+          content: messageText,
+        },
+      ];
+      return newChatMessages;
+    });
     console.log("Message text:", messageText);
     const response = await fetch(`/api/chat/sendMessage`, {
       method: "POST",
@@ -34,7 +49,18 @@ export default function ChatPage() {
       <div className="grid h-screen grid-cols-[260px_1fr] ">
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
-          <div className="flex-1 text-white">{incomingMessage}</div>
+          <div className="flex-1 text-white">
+            {newChatMessages.map((message) => (
+              <Message
+                key={message._id}
+                role={message.role}
+                content={message.content}
+              />
+            ))}
+            {incomingMessage && (
+              <Message role="assistant" content={incomingMessage} />
+            )}
+          </div>
           <footer className="bg-gray-800 p-10 ">
             <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">

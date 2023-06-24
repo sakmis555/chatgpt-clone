a12beec3a9f262dc36dc9516bdca6dcbe8d36ebe
Saksham Mishra <sakmis555@gmail.com>
2023-06-24 18:52:17 +0530

    fix: styles of sidebar updated

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,3 +1,9 @@
+import {
+  faMessage,
+  faPlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -17,16 +23,28 @@ export const ChatSidebar = () => {
   }, []);
   return (
     <div className="flex flex-col overflow-hidden bg-gray-900 text-white">
-      <Link href="/chat">New chat</Link>
+      <Link
+        href="/chat"
+        className="side-menu-item bg-emerald-500 hover:bg-emerald-600"
+      >
+        <FontAwesomeIcon icon={faPlus} /> New chat
+      </Link>
       <div className="flex-1 overflow-auto bg-gray-950">
         {console.log(chatList.length)}
         {chatList.map((chat) => (
-          <Link href={`/chat/${chat._id}`} key={chat._id}>
+          <Link
+            href={`/chat/${chat._id}`}
+            key={chat._id}
+            className="side-menu-item"
+          >
+            <FontAwesomeIcon icon={faMessage} />
             {chat.title}
           </Link>
         ))}
       </div>
-      <Link href="/api/auth/logout">Logout</Link>
+      <Link href="/api/auth/logout" className="side-menu-item bg-red-500">
+        <FontAwesomeIcon icon={faRightFromBracket} /> Logout
+      </Link>
     </div>
   );
 };

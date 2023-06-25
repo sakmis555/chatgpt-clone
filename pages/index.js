18fa921779a443557809e892aca1005482bb9899
Saksham Mishra <sakmis555@gmail.com>
2023-06-25 11:14:56 +0530

    fix: home page UI

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { getSession } from "@auth0/nextjs-auth0";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
@@ -14,18 +16,27 @@ export default function Home() {
         <title>TalkGPT - Login or Signup</title>
       </Head>
       <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
-        <div>
-          {user && <Link href="/api/auth/logout">Logout</Link>}
-          {!user && (
-            <>
-              <Link className="btn" href="/api/auth/login">
-                Login
-              </Link>
-              <Link className="btn" href="/api/auth/signup">
-                Signup
-              </Link>
-            </>
-          )}
+        <div className="">
+          <div>
+            <FontAwesomeIcon
+              icon={faRobot}
+              className="mb-2 text-8xl text-emerald-200"
+            />
+          </div>
+          <h1 className="text-4xl font-bold">Welcome to TalkGPT</h1>
+          <p className="mt-2 text-lg">Login with your account to continue</p>
+          <div className="mt-4 flex justify-center gap-3">
+            {!user && (
+              <>
+                <Link className="btn" href="/api/auth/login">
+                  Login
+                </Link>
+                <Link className="btn" href="/api/auth/signup">
+                  Signup
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>

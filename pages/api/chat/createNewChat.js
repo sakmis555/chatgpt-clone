19ee3052198e20e498a70244bd852e2032ea2e05
Saksham Mishra <sakmis555@gmail.com>
2023-06-25 11:51:06 +0530

    fix: validate createNewChat enpoint

--- pages/api/chat/createNewChat.js
+++ pages/api/chat/createNewChat.js
@@ -2,29 +2,38 @@ import { getSession } from "@auth0/nextjs-auth0";
 import clientPromise from "lib/mongodb";
 
 export default async function handler(req, res) {
-    try {
-        const {user} = await getSession(req, res);
-        const {message} = req.body;
-        const newUserMessage = {
-            role: "user",
-            content: message,
-        };
-        const client = await clientPromise;
-        const db = client.db("TalkGPT");
-        const chat = await db.collection("chats").insertOne({
-            userId: user.sub,
-            messages: [newUserMessage],
-            title: message
-        });
-        res.status(200).json({
-            _id: chat.insertedId.toString(),
-            messages: [newUserMessage],
-            title: message,
-        });
-    } catch (e) {
-        res.status(500).json({
-            message: "An error occurred when creatin a new chat"
-        });
-        console.log("ERROR OCCURRED IN CREATE NEW CHAT:", e);
+  try {
+    const { user } = await getSession(req, res);
+    const { message } = req.body;
+
+    // validate message data
+    if (!message || typeof message !== "string" || message.length > 200) {
+      res.status(422).json({
+        message: "Message is required and must be less than 200 character",
+      });
+      return;
     }
-} 
+
+    const newUserMessage = {
+      role: "user",
+      content: message,
+    };
+    const client = await clientPromise;
+    const db = client.db("TalkGPT");
+    const chat = await db.collection("chats").insertOne({
+      userId: user.sub,
+      messages: [newUserMessage],
+      title: message,
+    });
+    res.status(200).json({
+      _id: chat.insertedId.toString(),
+      messages: [newUserMessage],
+      title: message,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "An error occurred when creatin a new chat",
+    });
+    console.log("ERROR OCCURRED IN CREATE NEW CHAT:", e);
+  }
+}
